ff0b9310ee7a99e8f862f44dbca7464f0b5dbfaf
sadhana631 <indhusweety631@gmail.com>
2024-08-16 07:14:58 +0000

    third commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
-const App = () => <div>Hello World</div>
+import './App.css'
 
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/not-found" component={NotFound} />
+    <Redirect to="not-found" />
+  </Switch>  
+)  
 export default App
